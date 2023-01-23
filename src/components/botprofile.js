f4d614785af1d4f7ba8bee449d21f83fac65bc96
Jeffy2k <jeffgichuki68@gmail.com>
2023-01-23 12:18:34 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/botprofile.js
@@ -0,0 +1,42 @@
+import { React,useEffect,useState }  from "react";
+import { NavLink } from "react-router-dom";
+import "../style/botProfile.css"
+import OneBot from "./OneBot";
+import { useHistory } from "react-router-dom"
+
+function BotProfile({id,image,name,phrase,health,armor,damage,addBot,handleId}) {
+  let [state,setState] = useState('')
+  let [added,setAdded] = useState(true)
+
+  const history = useHistory()
+
+ function deleteItem(){
+  console.log('deleted')
+  setAdded(false)
+ fetch (`https://json-server2-vercel.vercel.app/bots/${id}`,{
+  method : 'DELETE'})
+  .then(console.log('deletItem'))
+  history.push(`/`)
+}
+ 
+
+
+    return ( 
+      <div id="card">
+        <NavLink onClick={()=>{
+          handleId(id)
+        }} to = "/bot"  className = "hi">
+          <img src = {image}></img>
+          <h5 id ="botName">{name}</h5>
+          <h7 id = "botPhrase" >{phrase}</h7>
+         <span id="abilities" ><i id="health" className="material-icons">healing</i><h5 id="ability">{health}</h5><i id="shield" className="material-icons">shield</i><h5 id="ability">{armor}</h5><i id="bolt" className="material-icons">bolt</i><h5 id="ability">{damage}</h5>
+       </span>
+        </NavLink>
+          <button onClick = {()=>{
+            deleteItem()
+            }}id="delete">X</button>
+            </div>
+     );
+} 
+
+export default BotProfile;
